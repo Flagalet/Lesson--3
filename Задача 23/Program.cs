7f0701d6573f383b58f26bc097e752db78e1bae4
Dmitriy <zeybert90@mail.ru>
2022-10-10 18:52:32 +0300

    23

---
+++ Задача 23/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+void Kyb(int a)
+{
+    for (int i = 1; i <= a; i++)
+    {
+        Console.Write($"{i*i*i}");
+        if (i != a)
+        Console.Write($", ");
+    }
+}
+Console.Clear();
+Console.WriteLine("Введите число");
+int res = int.Parse(Console.ReadLine()!);
+Kyb(res);
+
